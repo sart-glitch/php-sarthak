4588b502845f44ea27caeb24e4a7875ed8554434
sart-glitch <75308102+sart-glitch@users.noreply.github.com>
2022-10-06 07:51:40 +0530

    Update config.php

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -1,7 +1,8 @@
 <?php
-$dbuser="root";
-$dbpass="";
-$host="localhost";
-$db="hostel";
-$mysqli =new mysqli($host,$dbuser, $dbpass, $db);
+$dbuser="user";
+$dbpass="test";
+$host="http://35.86.106.156:8000/";
+$db="myDb";
+#$mysqli =new mysqli($host,$dbuser, $dbpass, $db);
+$mysqli =new mysqli('db', 'user', 'test', "myDb");
 ?>
